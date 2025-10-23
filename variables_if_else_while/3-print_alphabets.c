fc2411a0c9050708f1666ad333dd55a232c667cf
Thomas-lab17 <vbizzle.minecraft@gmail.com>
2025-10-23 13:05:44 +0000

    tsk3 test1

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase, followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'Z'; letter++)
+	{
+	putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}
