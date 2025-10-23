e08dc2466eb9ee45d9b00f3e684319da77f83a8a
Thomas-lab17 <vbizzle.minecraft@gmail.com>
2025-10-23 12:01:10 +0000

    tsk1 test4

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -14,19 +14,19 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	Last_digit = n % 10;
+	Last_d = n % 10;
 	/* your code goes there */
-	if (Last_digit < 6 && Last_digit != 0)
+	if (Last_d < 6 && Last_d != 0)
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, Last_digit);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, Last_d);
 	}
-	if (Last_digit > 5)
+	if (Last_d > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5\n", n, Last_digit);
+		printf("Last digit of %d is %d and is greater than 5\n", n, Last_d);
 	}
-	if (Last_digit == 0)
+	if (Last_d == 0)
 	{
-		printf("Last digit of %d is %d and is 0\n", n, Last_digit);
+		printf("Last digit of %d is %d and is 0\n", n, Last_d);
 	}
 	return (0);
 }
