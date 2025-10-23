826381da669061248fbd5dc7bdce275c45dd3fd6
Thomas-lab17 <vbizzle.minecraft@gmail.com>
2025-10-23 12:49:59 +0000

    pls work

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -10,11 +10,13 @@
 
 int main(void)
 {
-	char letter = 'a';
-{
-	for(letter = 'a'; letter <= 'z'; letter++) {
-		putchar(letter);
+    char letter;
+
+    for (letter = 'a'; letter <= 'z'; letter++) {
+        putchar(letter);
+    }
+
+    putchar('\n');
+    return 0;
 }
-		putchar('\n');
-	return (0);
-}
+
