4a5b412ec5f2ee7dbc52a1dec3e26fb7dbe16638
Thomas-lab17 <vbizzle.minecraft@gmail.com>
2025-10-24 07:15:52 +0000

    tsk7 test1

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase, followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'z'; letter >= 'a'; letter--)
+	{
+	putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}
