0aef258e351f0f9a8e82949fef21b23107985189
Thomas-lab17 <vbizzle.minecraft@gmail.com>
2025-10-24 13:10:55 +0000

    tsk0 test1

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase, followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("_putchar");
+	return (0);
+}
