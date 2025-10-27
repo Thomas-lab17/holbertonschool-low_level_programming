de862425e37924f557788edbda85166bbb8a1b11
Thomas-lab17 <vbizzle.minecraft@gmail.com>
2025-10-27 10:00:55 +0000

    tsk0 betty style

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 #include "main.h"
-
 /**
- *
- *
- *
- *
- */
-
+  * main- entry point
+  *
+  * _isupper: return(1)
+  * c: c'est le c
+  */
 int _isupper(int c)
 {
 	if (c >= 65 && c <= 90)
