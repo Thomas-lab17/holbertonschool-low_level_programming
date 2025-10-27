aeea6b8e40c3dfa2ed6de958efe6ffbd58aee306
Thomas-lab17 <vbizzle.minecraft@gmail.com>
2025-10-27 15:00:03 +0000

    tsk5 test1

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+
+void more_numbers(void)
+{
+	int line, n;
+
+	for (line = 0; line < 10; line++)
+	{
+		for (n = 0; n <= 14; n++)
+		{
+			if (n >= 10)
+				_putchar((n / 10) + '0');
+			_putchar((n % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
