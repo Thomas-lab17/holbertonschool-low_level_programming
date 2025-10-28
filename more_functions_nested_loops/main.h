d898f2812509367c30bdc3ae715dd82e9efbda8e
Thomas-lab17 <vbizzle.minecraft@gmail.com>
2025-10-28 13:39:01 +0000

    tsk7 test1

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -7,6 +7,7 @@ int _isdigit(int c);
 int mul(int a, int b);
 void print_most_numbers(void);
 int _putchar(char c);
+void print_diagonal(int n);
 void print_numbers(void);
 void more_numbers(void);
 
