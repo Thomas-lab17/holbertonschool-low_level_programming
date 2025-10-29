db961aac8d26ec0ba97f1ea99a723fe82d2d92f2
Thomas-lab17 <vbizzle.minecraft@gmail.com>
2025-10-29 10:11:20 +0000

    tsk9 test3

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -4,20 +4,22 @@
 
 int main(void)
 {
-	int n;
-	{
-		for (n = 0; n <= 100; n++)
-		{
-			if (n % 15 == 0 )
-				printf("FizzBuzz");
-			else if(n % 3 == 0)
-				printf("Fizz");
-			else if(n % 5 == 0)
-				printf("Buzz");
-			if(n < 100)
-				putchar(' ');
-		}
-		putchar('\n');
-	}
-	return(0);
+    int i;
+
+    for (i = 1; i <= 100; i++)
+    {
+        if (i % 15 == 0)
+            printf("FizzBuzz");
+        else if (i % 3 == 0)
+            printf("Fizz");
+        else if (i % 5 == 0)
+            printf("Buzz");
+        else
+            printf("%d", i);
+
+        if (i < 100)
+            printf(" ");
+    }
+    printf("\n");
+    return (0);
 }
