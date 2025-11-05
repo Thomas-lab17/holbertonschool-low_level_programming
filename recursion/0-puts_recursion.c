699a54faa3193d3d1db014a22608ea147c0cc60d
Thomas-lab17 <vbizzle.minecraft@gmail.com>
2025-11-05 10:04:22 +0000

    ok

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,12 +1,12 @@
 #include "main.h"
 void _puts_recursion(char *s)
-{ 
-    if (*s)
-    {
-        _putchar('\n');
-        return;
-    }
-    _putchar(*s);
-    _puts_recursion(s + 1);
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
 
+	else
+		_putchar('\n');
 }
