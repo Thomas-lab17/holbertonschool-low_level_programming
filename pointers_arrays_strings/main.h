796ab136ff6ac0a122b5b9e86f7b2609090ede37
Thomas-lab17 <vbizzle.minecraft@gmail.com>
2025-11-07 19:09:22 +0000

    reroll

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,6 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
-
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
