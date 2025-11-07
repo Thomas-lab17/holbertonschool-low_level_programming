9d4cc37d3b85a8139da54d36096dabcc6f04b05e
Thomas-lab17 <vbizzle.minecraft@gmail.com>
2025-11-07 19:28:49 +0000

    getrighttothatmyfriend

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int count = 0;
+	int i, found;
+
+	while (*s)
+	{
+		found = 0;
+		for (i = 0; accept[i]; i++)
+		{
+			if (*s == accept[i])
+			{
+				found = 1;
+				break;
+			}
+		}
+		if (!found)
+			break;
+		count++;
+		s++;
+	}
+	return (count);
+}
+
