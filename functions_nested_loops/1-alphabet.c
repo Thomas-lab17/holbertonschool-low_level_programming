d38a74062212ea98c10907ebfe01dc3b3d7d7442
Thomas-lab17 <vbizzle.minecraft@gmail.com>
2025-11-21 16:52:35 +0000

    corrections

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -5,14 +5,12 @@
  *
  * Return: Always 0 (/////Success)
  */
-
 void print_alphabet(void)
 {
-	char print_alphabet = ('a');
-		while (print_alphabet <= 'z')
-		{
-		_putchar (print_alphabet);
-		print_alphabet++;
-		}
-		_putchar('\n');
-		}
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+
+	_putchar('\n');
+}
