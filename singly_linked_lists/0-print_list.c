a74b121ce35ebd43976ad6117d57ccf174337e4e
Thomas_lab17 <vbizzle.minecraft@gmail.com>
2025-12-03 09:53:24 +0100

    task 0 project 1

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,32 +1,24 @@
-#include "list.h"
 #include <stdio.h>
-
-size_t print_list(const list_t *h);
-
+#include "lists.h"
 /**
- * print_list - prints all the elements of a list_t list
- * @h: pointer to the head of the list
- *
- * Return: number of nodes
+ * print_list - prints all the elements of a list_t list.
+ * @h: singly linked list.
+ * Return: number of elements in the list.
  */
-
-
 
 size_t print_list(const list_t *h)
 {
-	size_t nodes = 0;
+	size_t nelem;
 
-	while (h)
+	nelem = 0;
+	while (h != NULL)
 	{
 		if (h->str == NULL)
-			printf("[0] (nil)\n");
-
+			printf("[%d] %s\n", 0, "(nil)");
 		else
 			printf("[%d] %s\n", h->len, h->str);
-
-		nodes++;
 		h = h->next;
+		nelem++;
 	}
-
-	return (nodes);
+	return (nelem);
 }
