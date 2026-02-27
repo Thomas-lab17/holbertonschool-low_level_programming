4ad4f979c392852419e61b37e5ff106f1f9b5cc6
Thomas-lab17 <vbizzle.minecraft@gmail.com>
2026-02-27 09:15:20 +0100

    feat: first try: first try of task 0 of the nested loop  project in C

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * 
+ * Description: this program prints the word: _putchar (it was done by Thomas Charier from holberton laval)
+ * 
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	return (0)
+}
