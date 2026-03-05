558f5ce5f58f10dd0e5eccbd0a3452f25e637a21
Thomas-lab17 <vbizzle.minecraft@gmail.com>
2026-03-05 16:33:41 +0100

    style: format code for consistency in _puts function

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,14 +1,19 @@
 #include "main.h"
 
+/**
+ * _puts - affiche une string suivie d'un saut de ligne
+ * @str: la string à afficher
+ * Return: void
+ */
 void _puts(char *str)
 {
-    int i = 0;
-    while (str[i] != '\0') 
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
 	{
-        _putchar(str[i]);
-        i++;
-    }
-    _putchar('\n');
-
-
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
 }
