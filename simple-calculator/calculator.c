20d927a19ffb8b7951a755f338cac205b3e6859d
Thomas-lab17 <vbizzle.minecraft@gmail.com>
2026-03-05 12:06:49 +0100

    feat: restructure calculator loop for improved readability and user interaction

--- simple-calculator/calculator.c
+++ simple-calculator/calculator.c
@@ -9,8 +9,9 @@ int main(void)
 	
 
 	printf("Simple Calculator\n");
-
-	printf("1) Add\n2) Subtract\n3) Multiply\n4) Divide\n0) Quit\n"); 
+	while(1)
+	{
+		printf("1) Add\n2) Subtract\n3) Multiply\n4) Divide\n0) Quit\n"); 
 	/**it works the same way, i have just made it in a single line, 
 	 *less readable but more condensed if you want a low number of lines
 	printf("2) Subtract\n");
@@ -18,15 +19,20 @@ int main(void)
 	printf("4) Divide\n");
 	printf("0) Quit\n");
 	*/
-	scanf("%d", &choice);
+	
+		scanf("%d", &choice);
 
-	printf("choice: %d\n", choice);
-
-	if(choice == 0)
-	{
-		printf("bye");
+		printf("choice: %d\n", choice);
+		if (choice > 4 || choice < 0)
+		{
+			printf("invalid choice\n");
+		}
+		if(choice == 0)
+		{
+			printf("bye");
+			return(0);
+		}
+	
 	}
-
-	printf("bye");
-	return(0);
+	
 }
