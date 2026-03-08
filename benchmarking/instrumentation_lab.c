21e129c1102ca981d8c35cf1c945b0de97413bc0
Thomas-lab17 <vbizzle.minecraft@gmail.com>
2026-03-08 23:01:20 +0100

    refactor: enhance dataset processing functions and improve code readability

--- benchmarking/instrumentation_lab.c
+++ benchmarking/instrumentation_lab.c
@@ -6,99 +6,107 @@
 
 static int dataset[DATASET_SIZE];
 
-static volatile unsigned long checksum_sink;
-
+/**
+ * next_value - generates the next pseudo-random value
+ * @state: pointer to the PRNG state
+ *
+ * Return: the next pseudo-random unsigned int
+ */
 static unsigned int next_value(unsigned int *state)
 {
-    *state = (*state * 1103515245u) + 12345u;
-    return *state;
+	*state = (*state * 1103515245u) + 12345u;
+	return (*state);
 }
 
+/**
+ * build_dataset - fills the dataset array with pseudo-random values
+ */
 static void build_dataset(void)
 {
-    unsigned int state;
-    int i;
+	unsigned int state;
+	int i;
 
-    state = SEED_VALUE;
+	state = SEED_VALUE;
 
-    for (i = 0; i < DATASET_SIZE; i++)
-        dataset[i] = (int)(next_value(&state) % 100000);
+	for (i = 0; i < DATASET_SIZE; i++)
+		dataset[i] = (int)(next_value(&state) % 100000);
 }
 
+/**
+ * process_dataset - transforms every element of the dataset in place
+ */
 static void process_dataset(void)
 {
-    int i;
-    int v;
+	int i;
+	int v;
 
-    for (i = 0; i < DATASET_SIZE; i++)
-    {
-        v = dataset[i];
-        v = (v * 3) + (v / 7) - (v % 11);
-        if (v < 0)
-            v = -v;
-        dataset[i] = v;
-    }
+	for (i = 0; i < DATASET_SIZE; i++) {
+		v = dataset[i];
+		v = (v * 3) + (v / 7) - (v % 11);
+		if (v < 0)
+			v = -v;
+		dataset[i] = v;
+	}
 }
 
+/**
+ * reduce_checksum - computes a hash-like checksum over the dataset
+ *
+ * Return: the accumulated checksum
+ */
 static unsigned long reduce_checksum(void)
 {
-    unsigned long sum;
-    int i;
+	unsigned long sum;
+	int i;
 
-    sum = 0;
-    for (i = 0; i < DATASET_SIZE; i++)
-        sum = (sum * 131ul) + (unsigned long)dataset[i];
+	sum = 0;
+	for (i = 0; i < DATASET_SIZE; i++)
+		sum = (sum * 131ul) + (unsigned long)dataset[i];
 
-    return sum;
+	return (sum);
 }
 
+/**
+ * main - instruments build, process, and reduce phases with clock()
+ *
+ * Return: Always 0
+ */
 int main(void)
 {
-    unsigned long checksum;
+	unsigned long checksum;
+	clock_t total_start, total_end;
+	clock_t build_start, build_end;
+	clock_t process_start, process_end;
+	clock_t reduce_start, reduce_end;
+	double total_sec, build_sec, process_sec, reduce_sec;
 
-    clock_t total_start;
-    clock_t total_end;
-    clock_t build_start;
-    clock_t build_end;
-    clock_t process_start;
-    clock_t process_end;
-    clock_t reduce_start;
-    clock_t reduce_end;
+	total_start = clock();
 
-    double total_seconds;
-    double build_seconds;
-    double process_seconds;
-    double reduce_seconds;
+	build_start = clock();
+	build_dataset();
+	build_end = clock();
 
-    total_start = clock();
+	process_start = clock();
+	process_dataset();
+	process_end = clock();
 
-    build_start = clock();
-    build_dataset();
-    build_end = clock();
+	reduce_start = clock();
+	checksum = reduce_checksum();
+	reduce_end = clock();
 
-    process_start = clock();
-    process_dataset();
-    process_end = clock();
+	total_end = clock();
 
-    reduce_start = clock();
-    checksum = reduce_checksum();
-    reduce_end = clock();
+	(void)checksum;
 
-    total_end = clock();
+	total_sec = (double)(total_end - total_start) / CLOCKS_PER_SEC;
+	build_sec = (double)(build_end - build_start) / CLOCKS_PER_SEC;
+	process_sec = (double)(process_end - process_start) / CLOCKS_PER_SEC;
+	reduce_sec = (double)(reduce_end - reduce_start) / CLOCKS_PER_SEC;
 
-    checksum_sink = checksum;
+	printf("TOTAL seconds: %.6f\n", total_sec);
+	printf("BUILD_DATA seconds: %.6f\n", build_sec);
+	printf("PROCESS seconds: %.6f\n", process_sec);
+	printf("REDUCE seconds: %.6f\n", reduce_sec);
 
-    total_seconds = (double)(total_end - total_start) / (double)CLOCKS_PER_SEC;
-    build_seconds = (double)(build_end - build_start) / (double)CLOCKS_PER_SEC;
-    process_seconds = (double)(process_end - process_start) /
-        (double)CLOCKS_PER_SEC;
-    reduce_seconds = (double)(reduce_end - reduce_start) /
-        (double)CLOCKS_PER_SEC;
-
-    printf("TOTAL seconds: %.6f\n", total_seconds);
-    printf("BUILD_DATA seconds: %.6f\n", build_seconds);
-    printf("PROCESS seconds: %.6f\n", process_seconds);
-    printf("REDUCE seconds: %.6f\n", reduce_seconds);
-
-    return 0;
+	return (0);
 }
