baa3c81ce46325f236fe859a349375ab8d43c546
Thomas-lab17 <vbizzle.minecraft@gmail.com>
2026-03-08 22:29:25 +0100

    fix: refactor _atoi function for improved logic and readability

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -3,13 +3,29 @@
 
 int _atoi(char *s)
 {
-	int ent = 0;
+	int i;
+	int h, p;
 
-	while (s[ent] != '\0')
+	h = 0;
+	p = -1;
+	for (i = 0; s[i] != '\0'; i++)
 	{
+		if (s[i] == '-')
+			p *= -1;
 
-		ent++;
+		if (s[i] > 47 && s[i] < 58)
+		{
+			if (h < 0)
+				h = (h * 10) - (s[i] - '0');
+			else
+				h = (s[i] - '0') * -1;
+
+			if (s[i + 1] < 48  ||  s[i + 1] > 57)
+				break;
+		}
 	}
+	if (p < 0)
+		h *= -1;
 
-	return(0);
+	return (h);
 }
