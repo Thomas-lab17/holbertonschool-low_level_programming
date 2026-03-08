33c2264f42b9449000381693a45be11c973b7b3e
Thomas-lab17 <vbizzle.minecraft@gmail.com>
2026-03-08 23:14:01 +0100

    fix: update argument handling in main function to use argv[0] for program name feat: implement _atoi function for string to integer conversion and add main function for summing positive numbers

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,5 +1,5 @@
-#include "main.h"
 #include <stdio.h>
+#include "main.h"
 
 /**
  * main - prints the name of the program
@@ -10,7 +10,6 @@
  */
 int main(int argc __attribute__((unused)), char *argv[])
 {
-	printf("%s\n", argv[0]);
-
+	printf("%s\n", *argv);
 	return (0);
 }
