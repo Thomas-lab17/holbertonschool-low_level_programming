fc931a68fbe372ab9b4a52dd8d306d5ce6e73a84
Thomas-lab17 <vbizzle.minecraft@gmail.com>
2026-03-10 17:14:24 +0100

    fix: format code and improve documentation in create_array function

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -1,35 +1,34 @@
 #include "main.h"
 
 /**
- * create_array - creates an array of chars initialized with a specific char
- * @size: the size of the array to create
- * @c: the char to initialize the array with
- *
- * Return: a pointer to the array, or NULL if it fails or size is 0
- */
+*create_array - creates an array of chars initialized with a specific char
+*@size: the size of the array to create
+*@c: the char to initialize the array with
+*
+*Return: a pointer to the array, or NULL if it fails or size is 0
+*/
 
- char *create_array(unsigned int size, char c)
+char *create_array(unsigned int size, char c)
 {
 	char *tableau;
 	unsigned int i;
 
-	if(size == 0)
+	if (size == 0)
 	{
-		return(NULL);
+		return (NULL);
 	}
 
-	tableau = malloc(sizeof(char)*size);
+	tableau = malloc(sizeof(char) * size);
 
-	if(tableau == 0)
+	if (tableau == 0)
 	{
-		return(NULL);
+		return (NULL);
 	}
 
-	for( i = 0; i < size; i++ )
+	for (i = 0; i < size; i++)
 	{
 		tableau[i] = c;
 	}
 
-	return tableau;
-
+	return (tableau);
 }
