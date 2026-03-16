d0d40f8407b54b4b5ed7beafb85945745d9f5f14
Thomas-lab17 <vbizzle.minecraft@gmail.com>
2026-03-16 10:29:11 +0100

    fix: standardize indentation and formatting in _strdup function

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -1,39 +1,39 @@
 #include "main.h"
 
 /**
- * _strdup - returns a pointer to a newly allocated copy of a string
- * @str: string to duplicate
- *
- * Return: pointer to duplicated string, or NULL on failure
- */
+* _strdup - returns a pointer to a newly allocated copy of a string
+* @str: string to duplicate
+*
+* Return: pointer to duplicated string, or NULL on failure
+*/
 
 char *_strdup(char *str)
 {
-    char *new;
-    unsigned int len = 0, index = 0;
+	char *new;
+	unsigned int len = 0, index = 0;
 
-    if (str == NULL)
-    {
-        return (NULL);
-    }
+	if (str == NULL)
+	{
+		return (NULL);
+	}
 
-    while (str[len] != '\0')
-    {
-        len++;
-    }
+	while (str[len] != '\0')
+	{
+		len++;
+	}
 
-    new = malloc(sizeof(char) * (len + 1));
+	new = malloc(sizeof(char) * (len + 1));
 
-    if (new == NULL)
-    {
-        return (NULL);
-    }
+	if (new == NULL)
+	{
+		return (NULL);
+	}
 
-    while (index <= len)
-    {
-        new[index] = str[index];
-        index++;
-    }
+	while (index <= len)
+	{
+		new[index] = str[index];
+		index++;
+	}
 
-    return (new);
+	return (new);
 }
