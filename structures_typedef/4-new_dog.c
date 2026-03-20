deb38b6e96868f4e6de55e4057a531edbb48ba10
Thomas-lab17 <vbizzle.minecraft@gmail.com>
2026-03-20 09:47:34 +0100

    refactor: replace string.h functions with custom implementations in new_dog function

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,5 +1,41 @@
 #include "dog.h"
 
+
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	return (i);
+}
+
+char *_strcpy(char *dest, char *src)
+{
+	int len, i;
+
+	len = 0;
+
+	while (src[len] != '\0')
+	{
+		len++;
+	}
+
+	for (i = 0; i < len; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
+
+
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *d;
@@ -10,16 +46,16 @@ dog_t *new_dog(char *name, float age, char *owner)
 	}
     
 
-	d->name = malloc(strlen(name) + 1);
+	d->name = malloc(_strlen(name) + 1);
 	if(d->name == NULL)
 	{
 		free(d);
 		return(NULL);
 	}
 	
-	strcpy(d->name, name);
+	_strcpy(d->name, name);
 
-	d->owner = malloc(strlen(owner) + 1);
+	d->owner = malloc(_strlen(owner) + 1);
 	if(d->owner == NULL)
 	{
 		free(d->name);
@@ -27,7 +63,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return(NULL);
 	}
 	
-	strcpy(d->owner, owner);
+	_strcpy(d->owner, owner);
 
 
 	 d->age = age;
