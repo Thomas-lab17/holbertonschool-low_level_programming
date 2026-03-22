2009c40a22ae7f190ffe9bf891eb977b104b3447
Thomas-lab17 <vbizzle.minecraft@gmail.com>
2026-03-22 14:17:24 +0100

    style: add int_index function to search for an integer in an array

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,31 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - searches for an integer
+ * @array: array to search in
+ * @size: size of the array
+ * @cmp: pointer to the comparing function
+ *
+ * Return: index of the first element for which
+ * the cmp function does not return 0, or -1 if no match is found
+ * or size is negative
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int index;
+
+	if (!cmp || array == NULL || size <= 0)
+	{
+		return (-1);
+	}
+
+	for (index = 0; index < size; index++)
+	{
+		if (cmp(array[index]))
+		{
+			return (index);
+		}
+	}
+
+	return (-1);
+}
