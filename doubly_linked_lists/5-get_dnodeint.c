5dd4d4b84d8c377fb0bb9410bfc5e74e0154d850
Thomas-lab17 <vbizzle.minecraft@gmail.com>
2026-04-02 14:22:20 +0200

    fix: update function documentation for get_dnodeint_at_index to improve clarity

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -1,6 +1,12 @@
 #include "lists.h"
 
-
+/**
+ * get_dnodeint_at_index - returns the nth node of a dlistint_t linked list
+ * @head: pointer to the head of the list
+ * @index: index of the node, starting from 0
+ *
+ * Return: pointer to the nth node, or NULL if it does not exist
+ */
 
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
