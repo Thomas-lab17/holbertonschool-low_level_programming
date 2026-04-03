90ba7a63a3434ed066bc63ecfc2911c637efc2e1
Thomas-lab17 <vbizzle.minecraft@gmail.com>
2026-04-03 09:24:32 +0200

    feat: implement insert_dnodeint_at_index function to insert a new node at a specific position in a doubly linked list

---
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,58 @@
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - inserts a new node at a given position in a
+ * dlistint_t list
+ * @h: pointer to the head of the list
+ * @idx: index where the new node should be added, starting from 0
+ * @n: integer to be added to the new node
+ *
+ * Return: address of the new node, or NULL if it failed or if it is not
+ * possible to add the new node at index idx
+ */
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *new_node, *current;
+	unsigned int i;
+
+	if (h == NULL)
+		return (NULL);
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->prev = NULL;
+	new_node->next = NULL;
+
+	if (idx == 0)
+	{
+		new_node->next = *h;
+		if (*h != NULL)
+			(*h)->prev = new_node;
+		*h = new_node;
+		return (new_node);
+	}
+
+	current = *h;
+	for (i = 0; current != NULL && i < idx - 1; i++)
+		current = current->next;
+
+	if (current == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->next = current->next;
+	new_node->prev = current;
+
+	if (current->next != NULL)
+		current->next->prev = new_node;
+
+	current->next = new_node;
+
+	return (new_node);
+}
