690d010a10aa0a6415369f6df37837a1ecec5256
Thomas-lab17 <vbizzle.minecraft@gmail.com>
2026-04-03 11:45:35 +0200

    added task 3 of hash tables

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -1,31 +1,4 @@
 #include "hash_tables.h"
-
-/**
- * string_duplicate - duplicates a string into newly allocated memory
- * @str: string to duplicate
- *
- * Return: pointer to the duplicated string, or NULL on failure
- */
-static char *string_duplicate(const char *str)
-{
-	char *dup;
-	size_t len;
-	size_t i;
-
-	if (str == NULL)
-		return (NULL);
-	len = strlen(str);
-	dup = malloc(len + 1);
-	if (dup == NULL)
-		return (NULL);
-
-	for (i = 0; i < len; i++)
-		dup[i] = str[i];
-	dup[len] = '\0';
-
-	return (dup);
-}
-
 /**
  * hash_table_set - adds an element to the hash table
  * @ht: hash table
@@ -52,10 +25,10 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	{
 		if (strcmp(node->key, key) == 0)
 		{
-			dup_value = string_duplicate(value);
+			dup_value = strdup(value);
 			if (dup_value == NULL)
 				return (0);
-
+ 
 			free(node->value);
 			node->value = dup_value;
 			return (1);
@@ -63,10 +36,10 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		node = node->next;
 	}
 
-	dup_key = string_duplicate(key);
+	dup_key = strdup(key);
 	if (dup_key == NULL)
 		return (0);
-	dup_value = string_duplicate(value);
+	dup_value = strdup(value);
 	if (dup_value == NULL)
 	{
 		free(dup_key);
